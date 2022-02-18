8dfabc4c4ab4ae7f72736e0c1c05c40c612f47d8
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-02-19 00:02:56 +0100

    Update italian localization

--- src/parolottero_it_IT.ts
+++ src/parolottero_it_IT.ts
@@ -1,3 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it_IT"></TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>LangSelector</name>
+    <message>
+        <location filename="ui/LangSelector.qml" line="40"/>
+        <source>Pick a language</source>
+        <translation>Scegliere una lingua</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="64"/>
+        <source>Seed</source>
+        <translation>Seme</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="76"/>
+        <source>Duration</source>
+        <translation>Durata</translation>
+    </message>
+</context>
+<context>
+    <name>PlayBoard</name>
+    <message>
+        <location filename="ui/PlayBoard.qml" line="94"/>
+        <source>←</source>
+        <translation>←</translation>
+    </message>
+    <message>
+        <location filename="ui/PlayBoard.qml" line="134"/>
+        <source>Total: </source>
+        <translation>Totale: </translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="ui/main.qml" line="29"/>
+        <source>Parolottero</source>
+        <translation>Parolottero</translation>
+    </message>
+</context>
+</TS>
