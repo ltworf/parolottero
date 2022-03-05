892996ff73dcd93ff912a7331d179cc1a6260281
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-03-05 13:14:02 +0100

    Update italian translation

--- src/parolottero_it_IT.ts
+++ src/parolottero_it_IT.ts
@@ -2,32 +2,70 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="it_IT">
 <context>
+    <name>About</name>
+    <message>
+        <location filename="ui/About.qml" line="35"/>
+        <source>Parolottero</source>
+        <translation>Parolottero</translation>
+    </message>
+    <message>
+        <location filename="ui/About.qml" line="42"/>
+        <source>Released under AGPL-3</source>
+        <translation>Rilasciato sotto AGPL-3</translation>
+    </message>
+    <message>
+        <location filename="ui/About.qml" line="55"/>
+        <source>&lt;a href=&quot;https://github.com/ltworf/parolottero&quot;&gt;Website&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://github.com/ltworf/parolottero&quot;&gt;Sito web&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="ui/About.qml" line="61"/>
+        <source>Done</source>
+        <translation>Fatto</translation>
+    </message>
+</context>
+<context>
     <name>LangSelector</name>
     <message>
-        <location filename="ui/LangSelector.qml" line="40"/>
+        <location filename="ui/LangSelector.qml" line="48"/>
         <source>Pick a language</source>
         <translation>Scegliere una lingua</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="64"/>
+        <location filename="ui/LangSelector.qml" line="75"/>
+        <source>User defined seed</source>
+        <translation>Seme definito dall&apos;utente</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="80"/>
         <source>Seed</source>
         <translation>Seme</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="76"/>
+        <location filename="ui/LangSelector.qml" line="93"/>
         <source>Duration</source>
         <translation>Durata</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="107"/>
+        <source>About</source>
+        <translation>Informazioni su</translation>
     </message>
 </context>
 <context>
     <name>PlayBoard</name>
     <message>
-        <location filename="ui/PlayBoard.qml" line="94"/>
+        <location filename="ui/PlayBoard.qml" line="77"/>
         <source>←</source>
         <translation>←</translation>
     </message>
     <message>
-        <location filename="ui/PlayBoard.qml" line="134"/>
+        <location filename="ui/PlayBoard.qml" line="90"/>
+        <source>Scores</source>
+        <translation>Punteggio</translation>
+    </message>
+    <message>
+        <location filename="ui/PlayBoard.qml" line="277"/>
         <source>Total: </source>
         <translation>Totale: </translation>
     </message>
@@ -37,6 +75,7 @@
     <message>
         <location filename="ui/main.qml" line="29"/>
         <source>Parolottero</source>
+        <translatorcomment>Parolottero</translatorcomment>
         <translation>Parolottero</translation>
     </message>
 </context>
