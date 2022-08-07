0653a8116e716f31e478778a0012bceaf5277760
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-08-07 14:33:22 +0200

    Update italian locale

--- src/parolottero_it_IT.ts
+++ src/parolottero_it_IT.ts
@@ -27,27 +27,63 @@
 <context>
     <name>LangSelector</name>
     <message>
-        <location filename="ui/LangSelector.qml" line="48"/>
+        <location filename="ui/LangSelector.qml" line="49"/>
         <source>Pick a language</source>
         <translation>Scegliere una lingua</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="75"/>
+        <location filename="ui/LangSelector.qml" line="90"/>
+        <location filename="ui/LangSelector.qml" line="117"/>
+        <source>Download more languages</source>
+        <translation>Scarica più lingue</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="94"/>
+        <source>Downloading…</source>
+        <translation>Scaricamento…</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="112"/>
+        <source>Download error</source>
+        <translation>Errore di scaricamento</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="141"/>
+        <source>Download: %1</source>
+        <translation>Scarica: %1</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="164"/>
+        <source>Error downloading: %1</source>
+        <translation>Errore scaricando: %1</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="166"/>
+        <source>Downloaded: %1</source>
+        <translation>Scaricato: %1</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="169"/>
+        <source>Downloading: %1</source>
+        <translation>Scaricando: %1</translation>
+    </message>
+    <message>
+        <location filename="ui/LangSelector.qml" line="194"/>
         <source>User defined seed</source>
         <translation>Seme definito dall&apos;utente</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="80"/>
+        <location filename="ui/LangSelector.qml" line="199"/>
         <source>Seed</source>
         <translation>Seme</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="93"/>
+        <location filename="ui/LangSelector.qml" line="214"/>
         <source>Duration</source>
         <translation>Durata</translation>
     </message>
     <message>
-        <location filename="ui/LangSelector.qml" line="107"/>
+        <location filename="ui/LangSelector.qml" line="230"/>
         <source>About</source>
         <translation>Informazioni su</translation>
     </message>
@@ -65,7 +101,7 @@
         <translation>Punteggio</translation>
     </message>
     <message>
-        <location filename="ui/PlayBoard.qml" line="277"/>
+        <location filename="ui/PlayBoard.qml" line="279"/>
         <source>Total: </source>
         <translation>Totale: </translation>
     </message>
